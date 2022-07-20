689f6099e19ec92dadcec1b0a3e0e7c6cde4b7bc
Magnapinna <109501416+Magnapinna@users.noreply.github.com>
2022-07-20 15:15:37 -0400

    loops and branching Challenges

--- Week2/Week2.cpp
+++ Week2/Week2.cpp
@@ -4,19 +4,21 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <vector>
 
 using namespace std;
-
 void challengeOne();
 void challengeTwo();
-
+bool challengeThree();
+void challengeFour();
 
 int main()
 {
 	//challengeOne();
-	challengeTwo();
+	//challengeTwo();
+	//cout << "Your date was" << (challengeThree()?"" :"n't") << " a leap year." << endl;
+	challengeFour();
 }
-
 
 void challengeOne() {
 	cout << "Insert 3 number to be summed" << endl;
@@ -37,4 +39,50 @@ void challengeTwo() {
 	int toCube;
 	cin >> toCube;
 	cout << toCube << "^3 = " << pow(toCube, 3) << endl;
+}
+
+bool challengeThree() {
+	cout << "Input a year you'd like to check" << endl;
+	int year;
+	cin >> year;
+
+	if (year % 400 == 0) {
+		return true;
+	}
+	if (year % 100 == 0) {
+		return false;
+	}
+	if (year % 4 == 0) {
+		return true;
+	}
+	return false;
+}
+
+void challengeFour() {
+	cout << "insert a positive number" << endl;
+	int positiveNumber;
+	cin >> positiveNumber;
+
+	if (cin.fail()) {
+		cout << "You didn't enter a number. Dying." << endl;
+	}
+
+	if (positiveNumber <= 0) {
+		cout << "number provided not positive. dying." << endl;
+		return;
+	}
+
+	vector<int> vec;
+
+	for (int i = 1; i <= positiveNumber; i++) {
+		if (positiveNumber % i == 0) {
+			vec.push_back(i);
+		}
+	}
+
+	cout << "The factors of " << positiveNumber << " are ";
+	for (int i : vec) {
+		cout << i << ' ';
+	}
+	cout << endl;
 }
